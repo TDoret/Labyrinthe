ad3c9dcda91ab1ef91b0d30d76a2d16525f36481
Thomas Doret <thomas.doret33@gmail.com>
2017-01-27 12:22:52 +0100

    add M/N param

--- main.c
+++ main.c
@@ -12,15 +12,17 @@ int main(int argc, char *argv[])
   	int shm;                  			// identifiant de la memoire partagee
   	int *adjM = NULL;      				// pointeur d'attachement shared memory == matrice d'adjacence
   	int value = 1;
+  	int rowMax = 4;
+  	int ColMax = 4
 
 	printf("%s\n","Hello World !");
 	shm = generateSHM(argc, argv[0], argv[1]);
 	printf("%s\n", "----Write In SHM---");
-	writeSHM(shm, adjM, value, 0, 0, 4);
+	writeSHM(shm, adjM, value, 0, 0, rowMax);
 	printf("%s\n", "----Read In SHM----");
-	readSHM(shm, adjM, 0, 0, 4);
+	readSHM(shm, adjM, 0, 0, rowMax);
 	destroySHM(shm);
-	generateMaze(3,4);
+	generateMaze(rowMax,ColMax);
 	printf("\n%s\n","Fin Programme");
 	
     return EXIT_SUCCESS;
